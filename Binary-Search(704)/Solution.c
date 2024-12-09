5d72c4bb046dc53df592319379bef779ecd08def
abiraami279 <156501823+abiraami279@users.noreply.github.com>
2024-12-09 23:21:43 +0530

    Create solution for binary search

---
+++ Binary-Search(704)/Solution.c
@@ -0,0 +1,22 @@
+int search(int* nums, int numsSize, int target) {
+    int start=0;
+    int end=numsSize-1;
+    while(start<=end)
+    {
+        int mid=(start+end)/2;
+        if(nums[mid]==target)
+        {
+           return mid;
+        }
+        else if(nums[mid]>target)
+        {
+           end=mid-1;
+        }
+        else
+        {
+           start=mid+1;
+        }
+
+    }
+    return -1;
+}
